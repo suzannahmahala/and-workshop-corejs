8fe83ce12606f333c1d2745f51a9fe80cda9e2d6
sjames <sjames@and.digital>
2017-10-31 11:51:16 +0000

    renamed out to filteredResults

--- 12_exercise.js
+++ 12_exercise.js
@@ -1,23 +1,23 @@
 /**
  *  AVAILABLE FILTERS:
  *  ["AVAILABLE_IMMEDIATELY", "FRESH_GRAD", "JUNIOR", "JAVASCRIPT", "PHP", "AWS", "REACT", "JAVA"]
- *   
+ *
  *  "AVAILABLE_IMMEDIATELY" and "FRESH_GRAD" will override all the other filters if specified
- * 
+ *
  *  if "AVAILABLE_IMMEDIATELY" and "FRESH_GRAD" are both specified as filter, "FRESH_GRAD" will be ignored
- * 
- * 
+ *
+ *
  *  Exercise: refactor this code
  *  - take care of naming variables
  *  - get rid of the 'for loops'
  *  - move it to modern JS!
  *  - oh, there are missing tests/scenario
- *  
+ *
  *   happy refactory :)
  */
 
 function filter(results, filters) {
-  var out = [];
+  var filteredResults = [];
   var resultsLength = results.length;
   var filterLength = filters.length;
   var hasOptions;
@@ -56,13 +56,13 @@ function filter(results, filters) {
         }
       }
       if (hasOptions) {
-        out.unshift(results[i]);
+        filteredResults.unshift(results[i]);
       }
     }
   } else {
-    out = results;
+    filteredResults = results;
   }
-  return out;
+  return filteredResults;
 }
 
 module.exports = filter;
